d093d033456fbfb732ceb2cdad7756c06debc238
Derrick Kay <82616888+derrickkay@users.noreply.github.com>
2022-01-21 15:48:24 +0000

    Update index3.js

--- index3.js
+++ index3.js
@@ -10,6 +10,28 @@ const randomNumber = Math.floor(Math.random() * 8);
 
 let eightBall = '';
 
+if (randomNumber === 0) {
+  console.log(eightball = 'It is certain');
+} else if (randomNumber=== 1) {
+  console.log('It is certain');
+} else if (randomNumber === 2) {
+  console.log('It is decidedly so');
+} else if (randomNumber === 3) {
+  console.log('Reply hazy try again');
+} else if (randomNumber === 4) {
+  console.log('Cannot predict now');
+} else if (randomNumber === 5) {
+  console.log('My sources say no');
+} else if (randomNumber === 6) {
+  console.log('Outlook not so good');
+} else if (randomNumber === 7) {
+  console.log('Do not count on it');
+} else {
+  
+}
+
+// switch statement example  
+/*
 switch (randomNumber) {
   case 0: 
     eightBall = 'It is certain';
@@ -38,3 +60,4 @@ switch (randomNumber) {
 }
 
 console.log(`The Eight Ball Answered: ${eightBall}`);
+*/
